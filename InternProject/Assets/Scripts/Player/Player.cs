187547a2b30b9e2df44cc3f98d1a127d9f3f9a4b
Tuttep Tanwattanakul <tutteptan@kkumail.com>
2021-05-10 18:29:20 +0700

    Added tank rotation

--- InternProject/Assets/Scripts/Player/Player.cs
+++ InternProject/Assets/Scripts/Player/Player.cs
@@ -7,11 +7,14 @@ public class Player : MonoBehaviour
     [Header("Attributes")]
     [SerializeField] private Rigidbody2D rb = null;
     [SerializeField] private float movementSpeed = 5f; // Read from ScriptableObjects later
+    [SerializeField] private float rotationSpeed = 3f;
 
     [Header("Input Settings")]
     [SerializeField] private float controllerDeadZone = 0.1f;
 
     private PlayerControls playerControls;
+    private float moveDirection;
+    private float rotateDirection;
 
     private void Awake()
     {
@@ -20,8 +23,13 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
+       /* // Tank Movements
         playerControls.Tank.Move.performed += _ => Move();
-        playerControls.Tank.Move.canceled += _ => ResetMovement();
+        playerControls.Tank.Move.canceled += _ => StopMovement();
+
+        // Tank rotation
+        playerControls.Tank.Rotate.performed += _ => Rotate();
+        // playerControls.Tank.Rotate.canceled += _ => StopRotating();*/
     }
 
     private void OnDisable()
@@ -36,21 +44,43 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        
+        ReadInputValues();
+
+        // If the inputs are less than the dead zone, ignore them.
+        if (Mathf.Abs(moveDirection) >= controllerDeadZone)
+        {
+            Move();
+        } else
+        {
+            StopMovement();
+        }
+
+        if (Mathf.Abs(rotateDirection) >= controllerDeadZone) Rotate();
+    }
+
+    private void ReadInputValues()
+    {
+        moveDirection = playerControls.Tank.Move.ReadValue<float>();
+        rotateDirection = playerControls.Tank.Rotate.ReadValue<float>();
     }
 
     private void Move()
     {
-        float moveDirection = playerControls.Tank.Move.ReadValue<float>();
-
-        // If the input is less than the dead zone, ignore it.
-        if (Mathf.Abs(moveDirection) < controllerDeadZone) { return; }
-
-        rb.velocity = new Vector2(rb.velocity.x, moveDirection * movementSpeed);
+        rb.velocity = (Vector2)transform.up * -moveDirection * movementSpeed;
     }
 
-    private void ResetMovement()
+    private void Rotate()
     {
-        rb.velocity = new Vector2(rb.velocity.x, Vector2.zero.y);
+        rb.MoveRotation(transform.rotation * Quaternion.Euler(0, 0, -rotateDirection * rotationSpeed));
     }
+
+    private void StopMovement()
+    {
+        rb.velocity = Vector2.zero;
+    }
+
+    /*private void StopRotating()
+    {
+
+    }*/
 }
