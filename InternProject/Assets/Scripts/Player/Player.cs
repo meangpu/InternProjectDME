dbc4180f0e12fc41e499c323ca0eaeb4e32e0984
Tuttep Tanwattanakul <tutteptan@kkumail.com>
2021-05-12 00:38:24 +0700

    Tank barrels follows the mouse cursor

--- InternProject/Assets/Scripts/Player/Player.cs
+++ InternProject/Assets/Scripts/Player/Player.cs
@@ -6,8 +6,8 @@ public class Player : MonoBehaviour
 {
     [Header("Attributes")]
     [SerializeField] private Rigidbody2D rb = null;
-    [SerializeField] private Transform gun = null;
-    [SerializeField] private Transform barrel = null;
+    [SerializeField] private Transform gun = null; // Gun Pivoting point
+    [SerializeField] private Transform barrel = null; // Bullet Spawn point
     [SerializeField] private float movementSpeed = 5f; // Read from ScriptableObjects later
     [SerializeField] private float rotationSpeed = 3f;
     [SerializeField] private Tank tank = null;
@@ -18,9 +18,13 @@ public class Player : MonoBehaviour
     [Header("Input Settings")]
     [SerializeField] private float controllerDeadZone = 0.1f;
 
+    private Camera mainCamera;
+
+    // Player Controls vars
     private PlayerControls playerControls;
     private float moveDirection;
     private float rotateDirection;
+    private Vector2 mousePos;
 
     private void Awake()
     {
@@ -29,6 +33,7 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
+        mainCamera = Camera.main;
         playerControls.Tank.Shoot.performed += _ => Shoot();
     }
 
@@ -45,6 +50,7 @@ public class Player : MonoBehaviour
     private void Update()
     {
         ReadInputValues();
+        RotateBarrel();
 
         // If the inputs are less than the dead zone, ignore them.
         if (Mathf.Abs(moveDirection) >= controllerDeadZone)
@@ -58,11 +64,11 @@ public class Player : MonoBehaviour
 
         if (Mathf.Abs(rotateDirection) >= controllerDeadZone)
         {
-            Rotate();
+            RotateTank();
         }
         else
         {
-            StopRotation();
+            StopTankRotation();
         }
     }
 
@@ -70,6 +76,7 @@ public class Player : MonoBehaviour
     {
         moveDirection = playerControls.Tank.Move.ReadValue<float>();
         rotateDirection = playerControls.Tank.Rotate.ReadValue<float>();
+        mousePos = playerControls.Tank.LookAt.ReadValue<Vector2>();
     }
 
     private void Move()
@@ -77,7 +84,7 @@ public class Player : MonoBehaviour
         rb.velocity = (Vector2)transform.up * -moveDirection * tank.moveSpeed;
     }
 
-    private void Rotate()
+    private void RotateTank()
     {
         rb.MoveRotation(transform.rotation * Quaternion.Euler(0, 0, -rotateDirection * rotationSpeed));
         // playerTank.Rotate(Vector3.forward * -rotateDirection * rotationSpeed);
@@ -88,7 +95,7 @@ public class Player : MonoBehaviour
         rb.velocity = Vector2.zero;
     }
 
-    private void StopRotation()
+    private void StopTankRotation()
     {
         rb.MoveRotation(transform.rotation * Quaternion.Euler(Vector3.zero));
         // playerTank.Rotate(Vector3.zero);
@@ -98,5 +105,15 @@ public class Player : MonoBehaviour
     {
         Instantiate(bulletPrefab, barrel.position, barrel.rotation);
     }
-}
 
+    private void RotateBarrel()
+    {
+        Vector2 cursorPosOnScreen = mainCamera.ScreenToWorldPoint(mousePos);
+
+        Vector3 gunDirection = (Vector3)cursorPosOnScreen - gun.position;
+
+        float aimAtAngle = Mathf.Atan2(gunDirection.y, gunDirection.x) * Mathf.Rad2Deg;
+
+        gun.rotation = Quaternion.RotateTowards(gun.rotation, Quaternion.Euler(0, 0, aimAtAngle+90), Mathf.Infinity);
+    }
+}
