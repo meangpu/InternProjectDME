ea2d3e81a92c8d357c645db017f1d118383c0d62
meangpu <51825872+meangpu@users.noreply.github.com>
2021-05-22 20:14:49 +0700

    prototype book scene

---
+++ InternProject/Assets/Scripts/SceneManager/CustomizeScene/TankCustomize.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class TankCustomize : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+}
