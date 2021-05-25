b6a6e75ec7eb3bcea5e5dea33d4d36f3bab4f4e9
meangpu <51825872+meangpu@users.noreply.github.com>
2021-05-25 16:23:25 +0700

    Update PlayerAbilities.cs

--- InternProject/Assets/Scripts/Player/PlayerAbilities.cs
+++ InternProject/Assets/Scripts/Player/PlayerAbilities.cs
@@ -75,7 +75,8 @@ public class PlayerAbilities : MonoBehaviour
         if (!playerStats.SpendEnergy(bombEnergyCost)) { return; }
 
         Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, bombRange);
-        CinemacineShake.Instance.ShakeCam(7f, 0.514f);
+
+        CinemacineShake.Instance.ShakeCam(7f, 0.514f); // shake screen
 
         foreach (Collider2D collider in enemies)
         {
