3eb4c681eb5ba12d4a4f382625cde2c5bfc662bd
Tuttep Tanwattanakul <tutteptan@kkumail.com>
2021-06-01 13:15:50 +0700

    Fixed stunned effect

--- InternProject/Assets/Scripts/UI/Ingame/AddonsDisplayIngame.cs
+++ InternProject/Assets/Scripts/UI/Ingame/AddonsDisplayIngame.cs
@@ -11,15 +11,74 @@ public class AddonsDisplayIngame : MonoBehaviour
     [SerializeField] private Image addonE = null;
     [SerializeField] private TMP_Text energyCostQText = null;
     [SerializeField] private TMP_Text energyCostEText = null;
+    [SerializeField] private Image rechargeBarQ = null;
+    [SerializeField] private Image rechargeBarE = null;
+    [SerializeField] private TMP_Text cooldownDurationQ = null;
+    [SerializeField] private TMP_Text cooldownDurationE = null;
+    [SerializeField] private CooldownSystem cooldownSystem = null;
+
+    private List<ObjAbility> addonsList;
+
+    private ObjAbility objQ;
+    private ObjAbility objE;
+
+    private float remainingCooldownQ = 0;
+    private float remainingCooldownE = 0;
+
+    private float remainingPercentageQ = 0;
+    private float remainingPercentageE = 0;
+
+
+    private void Awake()
+    {
+        addonsList = equippedAddons.GetEquippedAddons();
+
+        objQ = addonsList[0];
+        objE = addonsList[1];
+    }
 
     private void Start()
     {
-        List<ObjAbility> addonsList = equippedAddons.GetEquippedAddons();
+        addonQ.sprite = objQ.GetIcon();
+        addonE.sprite = objE.GetIcon();
 
-        addonQ.sprite = addonsList[0].GetIcon();
-        addonE.sprite = addonsList[1].GetIcon();
+        energyCostQText.text = objQ.GetEnergyCost().ToString();
+        energyCostEText.text = objE.GetEnergyCost().ToString();
 
-        energyCostQText.text = addonsList[0].GetEnergyCost().ToString();
-        energyCostEText.text = addonsList[1].GetEnergyCost().ToString();
+        ResetCooldownUI();
+    }
+
+    private void Update()
+    {
+        if (remainingPercentageQ != 0)
+        {
+            rechargeBarQ.fillAmount = remainingPercentageQ;
+        }
+        
+        if (remainingPercentageE != 0)
+        {
+            rechargeBarE.fillAmount = remainingPercentageE;
+        }
+
+        GetRemainingDuration();
+        CalculatePercentage();
+    }
+
+    private void GetRemainingDuration()
+    {
+        remainingCooldownQ = cooldownSystem.GetRemainingDuration(objQ.GetAbilityType());
+        remainingCooldownE = cooldownSystem.GetRemainingDuration(objE.GetAbilityType());
+    }
+
+    private void CalculatePercentage()
+    {
+        remainingPercentageQ = remainingCooldownQ / objQ.GetCooldown();
+        remainingPercentageE = remainingCooldownE / objE.GetCooldown();
+    }
+
+    private void ResetCooldownUI()
+    {
+        rechargeBarQ.fillAmount = remainingPercentageQ;
+        rechargeBarE.fillAmount = remainingPercentageE;
     }
 }
