b3a588171f861c1d2957cee6d3345bebb1065d90
Tuttep Tanwattanakul <tutteptan@kkumail.com>
2021-06-04 14:56:55 +0700

    Added enemyAI script

---
+++ InternProject/Assets/Scripts/Enemies/EnemyAI.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private Seeker seeker = null;
+    [SerializeField] private EnemyDisplay enemyDisplay = null;
+    [SerializeField] private Rigidbody2D rb = null;
+    [SerializeField] private float pathScanInterval = 0.5f;
+    [SerializeField] private float nextWayPointDistance = 1.2f;
+
+    private Transform playerBase = null;
+
+    private Transform currentTarget = null;
+    private Path path;
+    private float currentTime = 1f;
+    private int currentWaypoint = 0;
+    private bool isEnded = false;
+
+    private void Awake()
+    {
+        playerBase = GameObject.FindGameObjectWithTag("Base").transform;
+    }
+
+    private void Update()
+    {
+        TryGetNewPath(Time.deltaTime);
+
+        if (path == null) { return; }
+
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            if (isEnded) { goto Skip1; } 
+            isEnded = true;
+            Skip1:
+            return;
+        }
+        else
+        {
+            if (!isEnded) { goto Skip2; }
+            isEnded = false;
+        }
+
+        Skip2:
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        rb.velocity = direction * enemyDisplay.Speed;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        rb.rotation = angle * enemyDisplay.Speed;
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < nextWayPointDistance)
+        {
+            currentWaypoint++;
+        }
+    }
+
+    private void TryGetNewPath(float deltaTime)
+    {
+        currentTime += deltaTime;
+
+        if (currentTime < pathScanInterval) { return; }
+
+        seeker.StartPath(rb.position, playerBase.position, OnPathComplete);
+        currentTime = 0f; 
+    }
+
+    private void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+}
