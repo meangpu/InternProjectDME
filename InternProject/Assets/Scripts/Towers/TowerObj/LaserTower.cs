db1eb8aaf9b7521655e254e4af419755d7a59fa7
Tuttep Tanwattanakul <tutteptan@kkumail.com>
2021-06-08 12:59:39 +0700

    Improve effects on laser tower

--- InternProject/Assets/Scripts/Towers/TowerObj/LaserTower.cs
+++ InternProject/Assets/Scripts/Towers/TowerObj/LaserTower.cs
@@ -9,10 +9,15 @@ public class LaserTower : MonoBehaviour, ITower
     [SerializeField] private TowerAI towerAI = null;
     [SerializeField] private Transform laserSpawnPoint = null;
 
+    [SerializeField] private GameObject startVFX = null;
+    [SerializeField] private GameObject endVFX = null;
+
     private Enemy target;
+    private List<ParticleSystem> particles = new List<ParticleSystem>();
 
     private void Awake()
-    {
+    {   
+        FillLists();
         DisableLaser();
     }
 
@@ -39,13 +44,22 @@ public class LaserTower : MonoBehaviour, ITower
         if (lineRenderer.enabled) { return; }
 
         lineRenderer.enabled = true;
+
+        for (int i = 0; i < particles.Count; i++)
+        {
+            particles[i].Play();
+        }
     }
 
     private void UpdateLaser()
     {
-        lineRenderer.SetPosition(0, laserSpawnPoint.position);
+        lineRenderer.SetPosition(0, (Vector2)laserSpawnPoint.position);
+        startVFX.transform.position = (Vector2)laserSpawnPoint.position;
+        startVFX.transform.rotation = laserSpawnPoint.rotation;
 
-        lineRenderer.SetPosition(1, target.GetTransform().position);
+        lineRenderer.SetPosition(1, (Vector2)target.GetTransform().position);
+        endVFX.transform.position = lineRenderer.GetPosition(1);
+        startVFX.transform.rotation = Quaternion.Inverse(laserSpawnPoint.rotation);
     }
 
     private void DisableLaser()
@@ -53,6 +67,32 @@ public class LaserTower : MonoBehaviour, ITower
         if (!lineRenderer.enabled) { return; }
 
         lineRenderer.enabled = false;
+
+        for (int i = 0; i < particles.Count; i++)
+        {
+            particles[i].Stop();
+        }
+    }
+
+    private void FillLists()
+    {
+        for (int i = 0; i < startVFX.transform.childCount; i++)
+        {
+            ParticleSystem particle = startVFX.transform.GetChild(i).GetComponent<ParticleSystem>();
+            if (particle != null)
+            {
+                particles.Add(particle);
+            }
+        }
+
+        for (int i = 0; i < endVFX.transform.childCount; i++)
+        {
+            ParticleSystem particle = endVFX.transform.GetChild(i).GetComponent<ParticleSystem>();
+            if (particle != null)
+            {
+                particles.Add(particle);
+            }
+        }
     }
 
     private void OnDestroy()
